70496f6b0f0e32af8a5ff1000bae1f501ecf3fe3
Karisha <40266227+karinadesu@users.noreply.github.com>
2021-02-01 16:58:32 +0300

    Create module.py

---
+++ module.py
@@ -0,0 +1,13 @@
+def main(**kwargs):
+    """
+    Это вызываемая функция.
+    Она будет выполняться для каждого отдельного запроса.
+
+    @param kwargs - входные параметры
+
+    This is function to execute.
+    It will be invoked for each request.
+
+    @param kwargs - input parameters
+    """
+    return f"Hello, {kwargs}!"

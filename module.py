61a40bf0032a517ba105618fc1b6d997bc5b1b20
Karisha <40266227+karinadesu@users.noreply.github.com>
2021-02-02 12:16:41 +0300

    Update module.py

--- module.py
+++ module.py
@@ -1,13 +1,28 @@
-def main(**kwargs):
-    """
-    Это вызываемая функция.
-    Она будет выполняться для каждого отдельного запроса.
+import os
 
-    @param kwargs - входные параметры
+def fun_env_var_and_param(param):
+    x = os.environ.get('VALUE')
+    print(param)
+    print(x)
+    resp = str(param)+'somesting'+str(x)
+    print(resp)
+    return resp
 
-    This is function to execute.
-    It will be invoked for each request.
+def with_env_var():
+    x = os.environ.get('VALUE')
+    y = int(x)*2
+    print (y)
+    return y
 
-    @param kwargs - input parameters
-    """
-    return f"Hello, {kwargs}!"
+def with_input_param(a, b):
+    c = a + b
+    print(c)
+    return c
+
+def without_input_param():
+    status_code = 'hello'
+    return status_code
+
+def test_status_d(name, bu):
+    return {'status_code': 201, "some": "schit", "bull": "shit",
+            'headers': {"Test_header": "test_value"}, 'query': {'v':1}}
